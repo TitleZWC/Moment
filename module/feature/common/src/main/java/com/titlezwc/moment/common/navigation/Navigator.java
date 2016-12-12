190b6c6a4dcdb4089f77dd2f094c8a33cbb05671
zwc <wenchao_android@163.com>
2016-12-13 07:36:28 +0800

    修改包名

--- module/feature/common/src/main/java/com/titlezwc/moment/common/navigation/Navigator.java
+++ module/feature/common/src/main/java/com/titlezwc/moment/common/navigation/Navigator.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 
+import com.titlezwc.common.constant.AnimType;
 import com.titlezwc.log.LogUtils;
 
 import javax.inject.Inject;
@@ -61,46 +62,55 @@ public class Navigator {
         }
     }
 
-//    public void navigateToOtherPage(@NonNull Context context, Class toPageClass, Intent intent ) {
-//        if (null == intent) {
-//            intent = new Intent();
-//        }
-//        intent.setClass(context, toPageClass);
-//        context.startActivity(intent);
-//    }
-//
-//    @Deprecated
-//    public void navigateToOtherPage(@NonNull Context context, String toPageClassName, Intent intent) {
-//        Class toPageClass = getClass(toPageClassName);
-//        if (null != context && null != toPageClass) {
-//            LogUtils.d("from = {} to = {}", context, toPageClassName);
-//            navigateToOtherPage(context, toPageClass, intent);
-//        } else {
-//            LogUtils.e("from = {} to = {}", context, toPageClassName);
-//        }
-//    }
-//
-//    @Deprecated
-//    public void navigateToOtherPageForResult(Activity activity, Class toPageClass, Intent intent, int requestCode) {
-//        if (null != activity) {
-//            intent.setClass(activity, toPageClass);
-//            activity.startActivityForResult(intent, requestCode);
-//        } else {
-//            LogUtils.e("from = {} to = {}", activity, toPageClass);
-//        }
-//    }
-//
-//    @Deprecated
-//    public void navigateToOtherPageForResult(Activity activity, String toPageClassName, Intent intent, int requestCode) {
-//        Class toPageClass = getClass(toPageClassName);
-//        if (null != activity && null != toPageClass) {
-//            LogUtils.d("from = {} to = {}", activity, toPageClassName);
-//            intent.setClass(activity, toPageClass);
-//            navigateToOtherPageForResult(activity, toPageClass, intent, requestCode);
-//        } else {
-//            LogUtils.e("from = {} to = {}", activity, toPageClassName);
-//        }
-//    }
+    public void navigateToOtherPage(@NonNull Context context, Class toPageClass, Intent intent, int animType) {
+        navigateToOtherPage(context, toPageClass, intent);
+        if (context instanceof Activity) {
+            withAnim((Activity) context, animType);
+        }
+    }
+
+    public void navigateToOtherPage(@NonNull Context context, String toPageClassName, Intent intent, int animType) {
+        navigateToOtherPage(context, toPageClassName, intent);
+        if (context instanceof Activity) {
+            withAnim((Activity) context, animType);
+        }
+    }
+
+    public void navigateToOtherPageForResult(Activity activity, Class toPageClass, Intent intent, int requestCode, int animType) {
+        if (null != activity) {
+            navigateToOtherPageForResult(activity, toPageClass, intent, requestCode);
+            withAnim( activity, animType);
+        } else {
+            LogUtils.e("from = {} to = {}", activity, toPageClass);
+        }
+    }
+
+    public void navigateToOtherPageForResult(Activity activity, String toPageClassName, Intent intent, int requestCode, int animType) {
+        if (null != activity) {
+            navigateToOtherPageForResult(activity, toPageClassName, intent, requestCode);
+            withAnim( activity, animType);
+        } else {
+            LogUtils.e("from = {} to = {}", activity, toPageClassName);
+        }
+    }
+
+    private void withAnim(Activity activity, int animType) {
+        switch (animType) {
+            case AnimType.TYPE_TOP2DOWN:
+                activity.overridePendingTransition(3, 7);
+                break;
+            case AnimType.TYPE_LEFT2RIGHT:
+                activity.overridePendingTransition(3, 7);
+                break;
+            case AnimType.TYPE_DOWN2TOP:
+                activity.overridePendingTransition(3, 7);
+                break;
+            case AnimType.TYPE_RIGHT2LEFT:
+                activity.overridePendingTransition(3, 7);
+                break;
+        }
+    }
+
     private Class getClass(String classFullName) {
         try {
             return Class.forName(classFullName);
